a4c2a2cabd89eeba3b1b1e93008c4d994ca42e3b
Yojans <yojansmelendez3@gmail.com>
2025-08-26 17:01:34 -0600

    Primeras modificaciones

--- app/src/main/java/com/example/fianzas/MainActivity.kt
+++ app/src/main/java/com/example/fianzas/MainActivity.kt
@@ -14,13 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.BtnInicio.setOnClickListener {
-            startActivity(Intent(this@MainActivity, Splash::class.java))
+            startActivity(Intent(this@MainActivity, Login::class.java))
         }
     }
 }
